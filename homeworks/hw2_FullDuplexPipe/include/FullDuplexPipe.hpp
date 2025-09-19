379b176a6562d8b91fd0399fa62683020a77e33c
TolikRogov <tolik_rogov@bk.ru>
2025-09-19 12:19:40 +0300

    hw2

--- homeworks/hw2_FullDuplexPipe/include/FullDuplexPipe.hpp
+++ homeworks/hw2_FullDuplexPipe/include/FullDuplexPipe.hpp
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <stdio.h>
+
+#define BUF_SIZE 	4096
+#define READ_PIPE 	0
+#define WRITE_PIPE 	1
+
+#define FILE_NAME	"file.txt"
+#define FILE_NEW	"new_"
+
+#define PIPE_RCV(fd) Pipe->actions.rcv(Pipe, fd)
+#define PIPE_SND(fd) Pipe->actions.snd(Pipe, fd)
+
+typedef struct pPipe Pipe;
+typedef struct op_table Ops;
+
+typedef struct op_table {
+    size_t (*rcv)(Pipe *self, int fd);
+    size_t (*snd)(Pipe *self, int fd);
+} Ops;
+
+typedef struct pPipe {
+	char* data;
+	int fd_direct[2];
+	int fd_back[2];
+	size_t len;
+	size_t size;
+	Ops actions;
+} Pipe;
--- homeworks/hw2_FullDuplexPipe/src/main.cpp
+++ homeworks/hw2_FullDuplexPipe/src/main.cpp
@@ -1,9 +1,92 @@
 #include <stdio.h>
-#include <FullDuplexPipe.hpp>
+#include <sys/fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include "FullDuplexPipe.hpp"
+
+int create_pipe(pPipe* Pipe, size_t buf_size);
+int destroy_pipe(pPipe* Pipe);
+
+static int child_run(pPipe* Pipe);
+static int parent_run(pPipe* Pipe);
 
 int main() {
 
-	printf("Hello, world!\n");
+    pPipe Pipe = {};
+    if (create_pipe(&Pipe, BUF_SIZE))
+        return 1;
 
-	return 0;
- }
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork failed");
+        return 1;
+    }
+
+    if (pid == 0) {
+        child_run(&Pipe);
+        return 1;
+    }
+    else {
+        if (parent_run(&Pipe))
+            return 1;
+        wait(NULL);
+    }
+
+    destroy_pipe(&Pipe);
+
+    return 0;
+}
+
+static int parent_run(pPipe* Pipe) {
+
+    close(Pipe->fd_direct[READ_PIPE]);
+    close(Pipe->fd_back[WRITE_PIPE]);
+
+    dup2(Pipe->fd_direct[WRITE_PIPE], STDOUT_FILENO);
+    close(Pipe->fd_direct[WRITE_PIPE]);
+
+    dup2(Pipe->fd_back[READ_PIPE], STDIN_FILENO);
+    close(Pipe->fd_back[READ_PIPE]);
+
+    int new_file = open(FILE_NEW FILE_NAME, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    if (new_file < 0) {
+        perror("opening new file failed");
+        return 1;
+    }
+
+    int file = open(FILE_NAME, O_RDONLY | 0644);
+    if (file < 0) {
+        perror("opening file failed");
+        return 1;
+    }
+
+    while (PIPE_RCV(file) > 0) {
+        PIPE_SND(STDOUT_FILENO);
+        PIPE_RCV(STDIN_FILENO);
+        PIPE_SND(new_file);
+    }
+
+    close(file);
+    close(new_file);
+    fclose(stdout);
+
+    return 0;
+}
+
+static int child_run(pPipe* Pipe) {
+
+    close(Pipe->fd_direct[WRITE_PIPE]);
+    close(Pipe->fd_back[READ_PIPE]);
+
+    dup2(Pipe->fd_direct[READ_PIPE], STDIN_FILENO);
+    close(Pipe->fd_direct[READ_PIPE]);
+
+    dup2(Pipe->fd_back[WRITE_PIPE], STDOUT_FILENO);
+    close(Pipe->fd_back[WRITE_PIPE]);
+
+    while (PIPE_RCV(STDIN_FILENO) > 0)
+        PIPE_SND(STDOUT_FILENO);
+
+    return 0;
+}
