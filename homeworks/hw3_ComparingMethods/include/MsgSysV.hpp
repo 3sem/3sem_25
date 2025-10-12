5b8f6c53dc12bde59f005d3b9e7f25ace64b743c
Anatoliy Rogov <tolik_rogov@bk.ru>
2025-10-12 20:10:52 +0300

    utils

--- homeworks/hw3_ComparingMethods/include/MsgSysV.hpp
+++ homeworks/hw3_ComparingMethods/include/MsgSysV.hpp
@@ -1,14 +1,10 @@
 #pragma once
 
-#define BILLION		1000000000L
+#include "Utils.hpp"
 
-#define FILE_NAME	"file.txt"
-#define FILE_NEW	"new_"
-#define FILE_CREATE 	"time dd if=/dev/urandom of=" FILE_NAME " bs=1048576 count=1"
-
-#define MSQ_SIZE 	8192
+#define MSQ_SIZE 		8192
 #define MSQ_PERMISSIONS 0644
-#define MSQ_END 	"EOM"
+#define MSQ_END 		"EOM"
 
 struct message {
  	long mtype;
--- homeworks/hw3_ComparingMethods/include/ShmSysV.hpp
+++ homeworks/hw3_ComparingMethods/include/ShmSysV.hpp
@@ -1,12 +1,8 @@
 #pragma once
 
-#define BILLION		1000000000L
+#include "Utils.hpp"
 
-#define FILE_NAME	"file.txt"
-#define FILE_NEW	"new_"
-#define FILE_CREATE 	"time dd if=/dev/urandom of=" FILE_NAME " bs=1048576 count=1"
-
-#define SHM_SIZE	8192
+#define SHM_SIZE		8192
 #define SHM_PERMISSIONS 0644
 
 struct SharedMemory {
